c7906fd4aecb625a73825e6a4fefaaf06cf26d40
Nic <nichostst@gmail.com>
2025-03-10 21:20:22 +0700

    Initial commit

---
+++ src/generatepage.py
@@ -0,0 +1,51 @@
+from markdown_blocks import markdown_to_html_node
+from inline_markdown import extract_title
+import glob
+import os
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+
+    with open(from_path, 'r') as s:
+        source = s.read()
+
+    with open(template_path, 'r') as s:
+        template = s.read()
+    
+    source_html = markdown_to_html_node(source).to_html()
+    title = extract_title(source)
+
+    out = template.replace("{{ Title }}", title).replace("{{ Content }}", source_html)
+
+    with open(dest_path, 'w') as o:
+        o.write(out)
+
+def write_file(fn, content):
+    structure = fn.split('/')[:-1]
+    for i in range(len(structure)):
+        dirname = '/'.join(structure[:i+1])
+        if not os.path.exists(dirname):
+            os.mkdir(dirname)
+    
+    with open(fn, 'w') as o:
+        o.write(content)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    mds = glob.glob(f'{dir_path_content}**/*.md', recursive=True)
+
+    with open(template_path, 'r') as s:
+        template = s.read()
+    
+    for md in mds:
+        path = dest_dir_path + '/'.join(md.split('/')[1:])[:-3] + '.html'
+
+        with open(md, 'r') as s:
+            source = s.read()
+
+
+        source_html = markdown_to_html_node(source).to_html()
+        title = extract_title(source)
+
+        out = template.replace("{{ Title }}", title).replace("{{ Content }}", source_html)
+        write_file(path, out)
